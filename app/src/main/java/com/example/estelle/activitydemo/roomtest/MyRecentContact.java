ada66a4538632033365224aef7714fa295cfa333
[wb.huhuimin] <[wb.huhuimin@mesg.corp.netease.com]>
2018-10-17 17:42:02 +0800

    update

---
+++ app/src/main/java/com/example/estelle/activitydemo/roomtest/MyRecentContact.java
@@ -0,0 +1,30 @@
+package com.example.estelle.activitydemo.roomtest;
+
+import java.util.Map;
+
+/**
+ * @author wb.huhuimin
+ * @Description: [功能描述]
+ * @Time: [序号]${date} ${time}
+ */
+public class MyRecentContact {
+    String ContactId;
+
+    String FromAccount;
+
+    String FromNick;
+
+    String RecentMessageId;
+
+    int UnreadCount;
+
+    String Content;
+
+    long Time;
+
+    long Tag;
+
+    Map<String, Object> Extension;
+
+
+}
---
+++ app/src/main/java/com/example/estelle/activitydemo/roomtest/RoomTestActivity.java
@@ -0,0 +1,11 @@
+package com.example.estelle.activitydemo.roomtest;
+
+import android.app.Application;
+
+/**
+ * @author wb.huhuimin
+ * @Description: [功能描述]
+ * @Time: [序号]${date} ${time}
+ */
+public class RoomTestActivity extends Application {
+}
